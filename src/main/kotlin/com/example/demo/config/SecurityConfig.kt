d1ef54019fa3f48e097fe969d1c10b835b902a2d
SantiagoLazo <selazo@sudamericano.edu.ec>
2024-01-16 21:02:47 -0500

    authentication

--- src/main/kotlin/com/example/demo/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/demo/config/SecurityConfig.kt
@@ -4,7 +4,6 @@ package com.example.demo.config
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
@@ -16,9 +15,9 @@ import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 
+
 @Configuration
 class SecurityConfig {
-
     @Autowired
     private val jwtFilter: JwtFilter? = null
 
@@ -26,13 +25,14 @@ class SecurityConfig {
     @Throws(Exception::class)
     open fun filterChain(http: HttpSecurity): SecurityFilterChain? {
         http
-            .csrf{csrf->csrf.disable()}
+            .csrf { csrf -> csrf.disable() }
             .cors(Customizer.withDefaults())
-            .sessionManagement{sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)}
-            .authorizeHttpRequests{authRequest->
+            .sessionManagement { sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { authRequest ->
                 authRequest
                     .requestMatchers("/auth/**").permitAll()
-                    .anyRequest().authenticated()
+
+                    .anyRequest().denyAll()
             }
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
 
